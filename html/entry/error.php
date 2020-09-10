187d335dc6c4f207f6806602f9d2584e6dd4340c
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2020-09-10 17:10:14 +0900

    errorページ作成(HTML)

---
+++ html/entry/error.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>プレエントリーお申し込み | QUELCODE ISA オンラインプログラミングスクール | 卒業まで学費不要、日本初のISAを採用</title>
+
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <img src="../images/cropped-quelcode-2.png" alt="QUELCODEロゴ" class="header-logo">
+  </header>
+
+  <main>
+    <div class="title">
+      <p class="main-title">
+        【ISA】QUELCODE<br>
+        プレエントリーフォーム
+      </p>
+      <p class="sub-title">
+        応募はこちらから。日本で初めてISA(学費後払い)を採用したプログラミングスクール<br>
+        の募集です。全国からのご応募をおまちしています。
+      </p>
+    </div>
+
+    <div class="error-message">
+      <p>エラーが発生しました。</p>
+    </div>
+  </main>
+
+</body>
+</html>

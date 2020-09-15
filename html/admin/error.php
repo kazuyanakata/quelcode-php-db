c73d24897bb7f6efc9c7e3c7d467c18afbc55f37
kazuyanakata <68310528+kazuyanakata@users.noreply.github.com>
2020-09-15 12:21:09 +0900

    errorページ作成(HTML)

---
+++ html/admin/error.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <ul class="header-list-items">
+      <li class="header-list"><a href="index.php" class="header-link">トップ</a></li>
+      <li class="header-list"><a href="data/list.php" class="header-link">申し込み</a></li>
+    </ul>
+  </header>
+
+  <main>
+    <p class="error">
+      エラーが発生しました
+    </p>
+  </main>
+</body>
+</html>
